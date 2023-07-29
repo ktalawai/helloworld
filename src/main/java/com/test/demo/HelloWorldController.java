94c646c2f47347ce7d685b8af0098258870c6840
16828 <kirankumar.talawai@compunneldigital.com>
2023-07-29 12:54:32 +0530

    3 Commit

--- src/main/java/com/test/demo/HelloWorldController.java
+++ src/main/java/com/test/demo/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("/hello")
+    @GetMapping("/hello") // Test Commit
     public String helloWorld() {
         return "Hello, World!";
     }
